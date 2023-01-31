992624c4a7a956c3954455153652c9bc96f7e146
jari <jarikruitbos14@gmail.com>
2023-01-31 13:30:34 +0100

    Added migration task

--- playarr-server/db/instance.ts
+++ playarr-server/db/instance.ts
@@ -1,7 +1,7 @@
-import {Kysely, Migration, Migrator, SqliteAdapter, SqliteIntrospector, SqliteQueryCompiler} from "kysely";
+import {Kysely, SqliteAdapter, SqliteIntrospector, SqliteQueryCompiler} from "kysely";
 import {Database as SqliteDatabase} from "sqlite";
 import {SqliteDriver} from "./driver.ts";
-import * as path from "std/path/mod.ts";
+
 
 import {ConfigTable} from "../config/db/tables.ts";
 
@@ -29,57 +29,5 @@ const db = new Kysely<Database>({
 		},
 	},
 });
-
-
-export async function migrate() {
-	const migrator = new Migrator({
-		db: db,
-		provider: {
-			getMigrations: async () => {
-				const migrations: Record<string, Migration> = {};
-				const root = Deno.cwd();
-
-				// First we get all the folders in the root directory
-				for await (const fileOrDirectory of Deno.readDir(root)) {
-					if (fileOrDirectory.isDirectory) {
-
-						// Check if this folder got a db/migrations folder
-						const migrationsDirectory = `${root}/${fileOrDirectory.name}/db/migrations`;
-						await Deno.stat(migrationsDirectory).then(async () => {
-
-							// If so, lets grab the migrations
-							const migrationFiles = await Deno.readDir(migrationsDirectory);
-
-							for await (const migration of migrationFiles) {
-								const {up, down} = await import(
-									path.join(migrationsDirectory, `./${migration.name}`)
-									);
-
-								migrations[migration.name] = {
-									up,
-									down,
-								};
-							}
-						}).catch(() => {
-							// No migrations found, but let's make the console happy
-						});
-					}
-				}
-
-				return migrations;
-			},
-		},
-	});
-
-	const {error, results} = await migrator.migrateToLatest();
-	if (results && results.length > 0) {
-		console.log(results);
-	}
-
-	if (error) {
-		const migrationError = error as Error;
-		throw migrationError.message;
-	}
-}
 
 export default db;
---
+++ playarr-server/db/migrate.ts
@@ -0,0 +1,61 @@
+import {Migration, Migrator} from "kysely";
+import * as path from "std/path/mod.ts";
+
+import db from "./instance.ts";
+
+export async function migrate() {
+	const migrator = new Migrator({
+		db: db,
+		provider: {
+			getMigrations: async () => {
+				const migrations: Record<string, Migration> = {};
+				const root = Deno.cwd();
+
+				// First we get all the folders in the root directory
+				for await (const fileOrDirectory of Deno.readDir(root)) {
+					if (fileOrDirectory.isDirectory) {
+
+						// Check if this folder got a db/migrations folder
+						const migrationsDirectory = `${root}/${fileOrDirectory.name}/db/migrations`;
+						await Deno.stat(migrationsDirectory).then(async () => {
+
+							// If so, lets grab the migrations
+							const migrationFiles = await Deno.readDir(migrationsDirectory);
+
+							for await (const migration of migrationFiles) {
+								const {up, down} = await import(
+									path.join(migrationsDirectory, `./${migration.name}`)
+									);
+
+								migrations[migration.name] = {
+									up,
+									down,
+								};
+							}
+						}).catch(() => {
+							// No migrations found, but let's make the console happy
+						});
+					}
+				}
+
+				return migrations;
+			},
+		},
+	});
+
+	const {error, results} = await migrator.migrateToLatest();
+	if (results && results.length > 0) {
+		console.log(results);
+	}
+
+	if (error) {
+		const migrationError = error as Error;
+		throw migrationError.message;
+	}
+}
+
+migrate().then(() => {
+	console.log("Finished migrating!");
+}).catch((e) => {
+	console.error(`Errors while migrating: ${e}`);
+});
