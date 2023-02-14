f8779f6f04fe7138645627dab9f0a65d47705b00
jari <jarikruitbos14@gmail.com>
2023-02-14 14:47:07 +0100

    Now load in env with export: true

--- playarr-server/startup/env.ts
+++ playarr-server/startup/env.ts
@@ -1,13 +1,5 @@
 import {load} from "dotenv";
 
 export async function setupEnv() {
-	const env = await load();
-
-	Object.entries(env).forEach((item) => {
-		const key = item[0];
-		const value = item[1];
-
-		console.log(`Loaded from .env file [${key}: ${value}]`);
-		Deno.env.set(key, value);
-	});
+	await load({allowEmptyValues: true, export: true});
 }
